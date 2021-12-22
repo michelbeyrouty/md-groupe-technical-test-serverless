669c617cff70a420e2cf76c8b5a6b7f579f88225
michelbeyrouty <michelbeyrouty@gmail.com>
2021-12-22 18:11:35 +0200

    Refactor code

--- controllers/expenses/create.js
+++ controllers/expenses/create.js
@@ -19,7 +19,9 @@ module.exports.handler = async (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      body: expense,
+      body: JSON.stringify({
+        expense,
+      }),
     };
 
     callback(null, response);
