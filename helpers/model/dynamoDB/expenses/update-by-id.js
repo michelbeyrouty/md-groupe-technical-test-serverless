55d8b73ba7cd2f594d580df6c30c6ef7d064b088
michelbeyrouty <michelbeyrouty@gmail.com>
2021-12-24 14:13:29 +0200

    Refactor code

--- helpers/model/dynamoDB/expenses/update-by-id.js
+++ helpers/model/dynamoDB/expenses/update-by-id.js
@@ -4,20 +4,31 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 const updateExpense = async (data) => {
 
+  // TODO: Add UpdateExpression
+
   const params = {
     TableName: 'expenses',
     Key: {
       'id': data.id,
     },
-    Item: {
-      'updatedAt': new Date().getTime(),
-      ...data,
-    },
   };
 
-  const result = await docClient.update(params).promise();
+  // const result = await docClient.update(params).promise();
 
-  return result.Item;
+  return {};
 };
 
 module.exports = updateExpense;
+
+// const params2 = {
+//   TableName: 'Table',
+//   Key: { HashKey : 'hashkey' },
+//   UpdateExpression: 'set #a = :x + :y',
+//   ConditionExpression: '#a < :MAX',
+//   ExpressionAttributeNames: {'#a' : 'Sum'},
+//   ExpressionAttributeValues: {
+//     ':x' : 20,
+//     ':y' : 45,
+//     ':MAX' : 100,
+//   }
+// }
